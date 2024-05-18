1e59b376ffff735fb2979363b8853e001cd98439
Gold Smith <gholdsmith@gmail.com>
2024-05-18 22:07:22 +0200

    template login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { ProductsComponent } from "./product/products/products.component";
 import { ProductsAdminComponent } from "./product/products-admin/products-admin.component";
 import { AuthGuard } from "./auth.guard";
+import { LoginComponent } from "./login/login.component";
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   { path: 'admin/products', component: ProductsAdminComponent, canActivate: [AuthGuard] },
   { path: 'products', component: ProductsComponent, canActivate: [AuthGuard]  },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +11,41 @@ export class LoginComponent implements OnInit {
   message: string = 'vous êtes déconnecté.';
   name: string;
   password: string;
+  auth: AuthService;
 
-  constructor() { } 
+  constructor(private authService: AuthService, private router: Router) { } 
 
   ngOnInit(): void {
+    this.auth = this.authService;
+  }
+
+ setMessage(){
+  if(this.auth.isLoggedIn){
+    this.message = 'Vous êtes connecté (admin/admin)';
+  } else {
+    this.message = 'Identifiant ou mot de passe incorrect.'
+  }
+
+ }
+
+  login() {
+   this.message = 'tentative de connexion en cours...';
+   this.auth.login(this.name, this.password).subscribe((isLoggedIn: boolean) => {
+    this.setMessage();
+    if(isLoggedIn){
+      this.router.navigate(['/products'])
+    } else {
+      this.password = '';
+      this.router.navigate(['/login'])
+    }
+   })
+  }
+
+  logout() {
+    // Logique de déconnexion
+    console.log('Logout');
+    this.auth.logout();
+    this.message = 'Vous êtes déconnecté';
   }
 
 }
