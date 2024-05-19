6f59cd968471e65c977c719ca0584b0a31b74d9d
Gold Smith <gholdsmith@gmail.com>
2024-05-19 07:44:55 +0200

    ajout du composant pTable

--- src/app/product/products-admin/products-admin.component.ts
+++ src/app/product/products-admin/products-admin.component.ts
@@ -1,22 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Product } from "../product.model";
 import { ProductsService } from "../products.service";
+import { SelectItem, FilterService, FilterMatchMode } from "primeng/api";
 
 @Component({
-  selector: 'app-products-admin',
-  templateUrl: './products-admin.component.html',
-  styleUrls: ['./products-admin.component.scss']
+  selector: "app-products-admin",
+  templateUrl: "./products-admin.component.html",
+  styleUrls: ["./products-admin.component.scss"],
 })
 export class ProductsAdminComponent implements OnInit {
   products: Product[] = [];
-  loading: boolean = true;
-    
-    constructor(private productsService: ProductsService) { }
+  cols: any[];
+
+  matchModeOptions: SelectItem[];
+
+  constructor(
+    private productsService: ProductsService,
+    private filterService: FilterService
+  ) {}
 
   ngOnInit(): void {
+
+    const customFilterName = 'custom-equals';
+
     this.productsService.getProducts().subscribe((data: Product[]) => {
       this.products = data;
     });
-  }
 
+
+    this.filterService.register(customFilterName, (value, filter): boolean => {
+      if (filter === undefined || filter === null || filter.trim() === '') {
+          return true;
+      }
+
+      if (value === undefined || value === null) {
+          return false;
+      }
+
+      return value.toString() === filter.toString();
+  });
+
+
+  this.cols = [
+    { field: "code", header: "code" },
+    { field: "name", header: "name" },
+    { field: "Action", header: "Action" },
+  ];
+
+  this.matchModeOptions = [
+    { label: "Custom Equals", value: customFilterName },
+    { label: "Starts With", value: FilterMatchMode.STARTS_WITH },
+    { label: "Contains", value: FilterMatchMode.CONTAINS }
+  ];
+  }
 }
